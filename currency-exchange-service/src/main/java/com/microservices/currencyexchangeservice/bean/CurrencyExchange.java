a6cc54cdbc55a88ae365024c1e07a354024d9f3d
davidYichengWei <a1093888167@hotmail.com>
2023-01-07 16:33:01 -0500

    Set up dynamic port in the resposne

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/bean/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/bean/CurrencyExchange.java
@@ -8,6 +8,8 @@ public class CurrencyExchange {
 	private String to;
 	private BigDecimal conversionMultiple;
 	
+	private String environment;
+	
 	// Needed for JPA
 	public CurrencyExchange() {
 		
@@ -51,6 +53,15 @@ public class CurrencyExchange {
 
 	public void setConversionMultiple(BigDecimal conversionMultiple) {
 		this.conversionMultiple = conversionMultiple;
+	}
+	
+
+	public String getEnvironment() {
+		return environment;
+	}
+
+	public void setEnvironment(String environment) {
+		this.environment = environment;
 	}
 
 	@Override
--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.microservices.currencyexchangeservice.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,20 @@ import com.microservices.currencyexchangeservice.bean.CurrencyExchange;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	@Autowired
+	// Interface representing the environment in which the current application is running. 
+	// Models two key aspects of the application environment: profiles and properties.
+	private Environment environment;
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-		return new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+		
+		CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+		
+		String port = environment.getProperty("local.server.port");
+		currencyExchange.setEnvironment(port);
+		
+		return currencyExchange;
 	}
 }
