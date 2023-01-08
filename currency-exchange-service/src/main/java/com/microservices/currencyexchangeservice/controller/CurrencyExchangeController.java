e67a2958effcfa71667588a0ef3db35ca69816ce
davidYichengWei <a1093888167@hotmail.com>
2023-01-08 15:11:32 -0500

    Connect currency-exchange service with Zipkin for distributed tracing

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.microservices.currencyexchangeservice.controller;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,10 +25,14 @@ public class CurrencyExchangeController {
 	
 	@Autowired
 	private CurrencyExchangeRepository repository;
+	
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	@CircuitBreaker(name = "default")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+		
+		logger.info("retrieveExchangeValue called with {} to {}", from, to);
 		
 		CurrencyExchange currencyExchange =repository.findByFromAndTo(from, to);
 		
