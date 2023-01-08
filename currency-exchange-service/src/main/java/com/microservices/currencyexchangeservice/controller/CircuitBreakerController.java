3e5f11bf5e9ed5d09067f6e92e0667eecad4f17f
davidYichengWei <a1093888167@hotmail.com>
2023-01-07 23:32:33 -0500

    Configure rate limiter and bulkhead

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -16,17 +18,20 @@ public class CircuitBreakerController {
 	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
 	@GetMapping("/sample-api")
-	//	@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+//	@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
 	// CircuitBreaker directly returns the default response without calling the service that is down after a number of requests
 	// 3 states of CircuitBreaker: close -> open -> half-open
-	@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+//  @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+//	@RateLimiter(name = "sample-api")
+	@Bulkhead(name = "default")
 	public String sampleApi() {
 		
-		logger.info("Sample Api call received");
-		
-		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/dummy-url", String.class);
-		
-		return forEntity.getBody();
+//		logger.info("Sample Api call received");
+//		
+//		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/dummy-url", String.class);
+//		
+//		return forEntity.getBody();
+		return "Sample api";
 	}
 	
 	public String hardcodedResponse(Exception ex) {
